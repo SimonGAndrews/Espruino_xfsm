0ce3390e843bb70b79dcd5e93e2b7012392bdc6a
Simon Andrews <55559464+SimonGAndrews@users.noreply.github.com>
2025-08-31 12:37:53 -0400

    Create src/jswrap_xfsm_v2_4.c as a copy of src/jswrap_xfsm.c

---
+++ src/jswrap_xfsm_v2_4.c
@@ -0,0 +1,9 @@
+// Content copied from src/jswrap_xfsm.c
+
+// Example code from src/jswrap_xfsm.c
+
+#include <stdio.h>
+
+void example_function() {
+    printf("This is an example function from jswrap_xfsm.c\n");
+}
