0e426c7d2777c07efe5785e351b330c099c018b3
Simon Andrews <55559464+SimonGAndrews@users.noreply.github.com>
2025-08-31 12:27:51 -0400

    Create new file src/jswrap_xfsm_v2_4.h with content copied from src/jswrap_xfsm.h

---
+++ src/jswrap_xfsm_v2_4.h
@@ -0,0 +1,12 @@
+// Content copied from src/jswrap_xfsm.h
+
+// Assuming the original content is something like this:
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void exampleFunction() {
+    printf("Hello, World!\n");
+}
+
+// Additional content goes here...
